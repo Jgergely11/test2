c1d34a99f2bf97fa3cb57bd6d0ef2b9477f15938
Jgergely11 <96205065+Jgergely11@users.noreply.github.com>
2023-09-08 15:58:37 -0400

    Update ref_download.sh

--- ref_download.sh
+++ ref_download.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-
-### Download all archives present in assembly list (shell script)
-
 for i in `cat ref_list.txt`; do curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/$i/download?include_annotation_type=CDS_FASTA&filename=$i.zip" -H "Accept: application/zip"; done
