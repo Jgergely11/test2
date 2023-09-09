cb6de1a9bdc8f6f742fc0bd0b615f76106ba2f98
Jgergely11 <96205065+Jgergely11@users.noreply.github.com>
2023-09-09 19:07:05 -0400

    Create parse_header4.sh

---
+++ parse_header4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in *; do 
+awk -i inplace '{if($0~/^>/) {print $1, $0}else {print $0}}' $i
+sed -i 's/_cds_.* >//' $i
+sed -i 's/>lcl/>ref/g' $i
+sed -i 's/cl|/|cds|/g' $i
+sed -i 's/_cds_/|cds|/g' $i
+sed -i 's/l|cds|/|cds|/g' $i; done
